10386ad1a551b8e058e217f7e0e52c9ba68053c8
programadorprincipiante <ivan.diaz.faundez@gmail.com>
2017-06-20 22:53:23 -0400

    no funk

--- validapuntos.php
+++ validapuntos.php
@@ -3,10 +3,6 @@ session_start();
 include 'conex.php';
 
 if(isset($_SESSION['AdminName'])) {
-
-
-
-
  ?>
 
  <html>
@@ -16,24 +12,26 @@ if(isset($_SESSION['AdminName'])) {
  	<meta charset="utf-8">
  </head>
  </head>
- <body>
- 		<?php
- 			include 'conex.php';
-      $nombre = $_POST['Nombre'];
- 			$puntos = $_POST['Puntos'];
+	<body>
+			<?php
+				include 'conex.php';
+				$nombre = $_POST['Nombre'];
+				$puntos = $_POST['Puntos'];
 			
-			$addpuntos = mysql_query("UPDATE usuario `Puntos`= `Puntos` + $puntos where Nombre = '".$nombre."'");
- 			/*if (mysql_num_rows($log)>0) {
- 			$row = mysql_fetch_array($log);
- 			$_SESSION["AdminName"] = $row['AdminName'];
- 				  	echo 'Iniciando sesión para '.$_SESSION['AdminName'].' <p>';
- 					echo '<script> window.location="inicioadmin.php"; </script>';
- 				}
- 				else{
- 					echo '<script> alert("Usuario o contraseña incorrectos.");</script>';
- 					echo '<script> window.location="ingresoadmin.php"; </script>';
- 				}
+				$addpuntos = mysql_query("UPDATE usuario `Puntos` = `Puntos` + '".$puntos."' where Nombre = '".$nombre."'");
+ 			
+			
+				/*if (mysql_num_rows($log)>0) {
+				$row = mysql_fetch_array($log);
+				$_SESSION["AdminName"] = $row['AdminName'];
+						echo 'Iniciando sesión para '.$_SESSION['AdminName'].' <p>';
+						echo '<script> window.location="inicioadmin.php"; </script>';
+					}
+					else{
+						echo '<script> alert("Usuario o contraseña incorrectos.");</script>';
+						echo '<script> window.location="ingresoadmin.php"; </script>';
+ 				
  			}*/
- 		?>
- </body>
+			?>
+	</body> 
  </html>
